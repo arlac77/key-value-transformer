c35cf2dbffb49221e6ff9024f7861c782ad7c346
Markus Felten <Markus.Felten@gmx.de>
2021-12-08 12:35:37 +0100

    refactor: prepare for different key value layouts

--- src/key-value-transformer.mjs
+++ src/key-value-transformer.mjs
@@ -15,6 +15,10 @@
  * @return {AsyncIterator<string>} lines with replaces key value pairs
  */
 export async function* keyValueTransformer(source, updates) {
+  const keyValueRegex = /^(\w+):\s*(.*)/;
+  const additionalValueRegex = /^\s+(.*)/;
+  const lineEnding = "\n";
+
   const presentKeys = new Set();
 
   let key, value;
@@ -22,36 +26,36 @@ export async function* keyValueTransformer(source, updates) {
   function* eject() {
     if (key !== undefined) {
       for (const [k, v] of updates(key, value, presentKeys)) {
-        yield `${k}: ${v}\n`;
+        yield `${k}: ${v}${lineEnding}`;
       }
       key = value = undefined;
     }
   }
 
   for await (const line of asLines(source)) {
-    const m = line.match(/^(\w+):\s*(.*)/);
+    const m = line.match(keyValueRegex);
     if (m) {
       yield* eject();
       key = m[1];
       value = m[2];
       presentKeys.add(key);
     } else if (key !== undefined) {
-      const m = line.match(/^\s+(.*)/);
+      const m = line.match(additionalValueRegex);
       if (m) {
         value += m[1];
       } else {
         yield* eject();
-        yield line + "\n";
+        yield line + lineEnding;
       }
     } else {
-      yield line + "\n";
+      yield line + lineEnding;
     }
   }
 
   yield* eject();
 
   for (const [k, v] of updates(undefined, undefined, presentKeys)) {
-    yield `${k}: ${v}\n`;
+    yield `${k}: ${v}${lineEnding}`;
   }
 }
 
