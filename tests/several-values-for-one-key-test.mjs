34d3d2d34a820cc9b454b6425e336e001aacba16
Markus Felten <Markus.Felten@gmx.de>
2022-03-24 14:44:46 +0100

    test: wip

--- tests/several-values-for-one-key-test.mjs
+++ tests/several-values-for-one-key-test.mjs
@@ -1,5 +1,5 @@
 import test from "ava";
-import { it, collect, identity } from "./helpers/util.mjs";
+import { it, collect } from "./helpers/util.mjs";
 
 import {
   keyValueTransformer,
@@ -7,11 +7,18 @@ import {
 } from "key-value-transformer";
 
 test.skip("same key sevaral times", async t => {
+
+  function* values(k, v) {
+    if (k !== undefined) {
+      yield [k, v];
+    }
+  }
+  
   t.is(
     await collect(
       keyValueTransformer(
         it(["Requires:", "p1 > 1.0\nRequires: p2 = 2.0"]),
-        identity,
+        values,
         equalSeparatedKeyValuePairOptions
       )
     ),
