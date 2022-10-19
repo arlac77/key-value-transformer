25350166357a617688793f5c404ca40db3bfe72b
Markus Felten <Markus.Felten@gmx.de>
2022-10-19 23:39:23 +0200

    add missing tests/deleted-content-entry-ava-node.mjs from template

---
+++ tests/deleted-content-entry-ava-node.mjs
@@ -0,0 +1,20 @@
+import test from "ava";
+import { DeletedContentEntry } from "content-entry";
+
+test("deleted content entry create", async t => {
+  const entry = new DeletedContentEntry("somewhere");
+  t.is(entry.name, "somewhere");
+  t.false(entry.isCollection);
+  t.true(entry.isBlob);
+  t.true(entry.isDeleted);
+  t.false(entry.isExistent);
+  t.true(entry.isEmpty);
+  t.deepEqual(JSON.parse(JSON.stringify(entry)), {
+    name: "somewhere",
+    isBlob: true,
+    isCollection: false
+  });
+  t.is(await entry.string, "");
+  t.is((await entry.buffer).length, 0);
+  t.is((await entry.readStream).read(), null);
+});
